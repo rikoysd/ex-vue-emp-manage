7aa3108ff4dfd8defb294e6fbb8dc6536524ebed
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-02-09 11:59:49 +0900

    src/routerのindex.tsファイルを修正

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,11 +4,11 @@ import VueRouter, { RouteConfig } from "vue-router";
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  // // パスが登録されていない場合はログイン画面へ(LoginAdmin.vueを作成したらコメントを外してください)
-  // {
-  //   path: "*",
-  //   component: () => import("../components/LoginAdmin.vue"),
-  // },
+  // パスが登録されていない場合はログイン画面へ(LoginAdmin.vueを作成したらコメントを外してください)
+  {
+    path: "/registerAdmin",
+    component: () => import("../components/RegisterAdmin.vue"),
+  },
 ];
 
 const router = new VueRouter({
