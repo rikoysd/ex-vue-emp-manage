e259a6e7fae5da422da2fe141aab0ca3da8e0848
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-02-09 15:30:45 +0900

    router/index.tsにloginAdminのpathを追加

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,10 +4,13 @@ import VueRouter, { RouteConfig } from "vue-router";
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  // パスが登録されていない場合はログイン画面へ(LoginAdmin.vueを作成したらコメントを外してください)
   {
     path: "/registerAdmin",
     component: () => import("../views/RegisterAdmin.vue"),
+  },
+  {
+    path: "/loginAdmin",
+    component: () => import("../views/LoginAdmin.vue"),
   },
 ];
 
