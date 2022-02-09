b9f4b864027cac9a8df0f18137f4aee3217a6075
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-02-09 17:04:24 +0900

    script要素の記述完了

--- src/views/EmployeeList.vue
+++ src/views/EmployeeList.vue
@@ -1,17 +1,41 @@
 <template>
-  <div>
-    
-  </div>
+  <div></div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-@Component
-export default class XXXComponent extends Vue {
+import { Employee } from "../types/employee";
+@Component({
+  components: {
+    EmployeeList,
+  },
+})
+export default class EmployeeList extends Vue {
+  /**
+   * Vuexストアのアクション経由で非同期でWebAPIから従業員一覧を取得する.
+   */
+  created(): void {
+    this.$store.dispatch("getEmployeeList");
+  }
 
+  /**
+   * 非同期で取得したVuexストア内の従業員数を取得し返す.
+   *
+   * @returns 従業員数
+   */
+  get employeeCount(): number {
+    return this.$store.getters.getEmployeeCount;
+  }
+
+  /**
+   * 非同期で取得したVuexストア内の従業員一覧を取得し返す.
+   *
+   * @returns 従業員一覧
+   */
+  get employees(): Array<Employee> {
+    return this.$store.getters.getEmployees;
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
